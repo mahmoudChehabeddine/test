c77596c2fd25942b6f466ea0a58690779810828c
Ubuntu <ubuntu@ip-172-31-26-41.eu-central-1.compute.internal>
2023-11-28 11:09:59 +0000

    Your descriptive commit message here

---
+++ moodledata/localcache/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// ********** This file is generated DO NOT EDIT **********
+$CFG->siteidentifier = 'OPUfDaAnp0aukefFS5ZUa3suFocycb6i54.93.220.232';
+$CFG->bootstraphash = '624b02fe7f775f6ef1107b8e193651d8';
+// Only if the file is not stale and has not been defined.
+if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
+    define('SYSCONTEXTID', 1);
+}
